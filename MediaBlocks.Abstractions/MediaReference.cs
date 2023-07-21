c5fa63911ae6137654dd11b6583a73a25caa96c7
Oleg Rakovitch <devgeslot@gmail.com>
2023-07-21 17:56:06 -0400

    Initial abstractions and tests

---
+++ MediaBlocks.Abstractions/MediaReference.cs
@@ -0,0 +1,7 @@
+﻿namespace Staticsoft.MediaBlocks.Abstractions;
+
+public class MediaReference
+{
+    public string Path { get; set; } = string.Empty;
+    public MediaType Type { get; set; } = MediaType.Unknown;
+}
