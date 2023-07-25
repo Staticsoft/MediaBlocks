31f42941f1cb0d8d660badc961a68688979099c0
Oleg Rakovitch <devgeslot@gmail.com>
2023-07-25 03:35:04 -0400

    Basic Operations (#2)

---
+++ MediaBlocks.Memory/TempIntermediateStorage.cs
@@ -0,0 +1,9 @@
+﻿using System.IO;
+
+namespace Staticsoft.MediaBlocks.Memory;
+
+public class TempIntermediateStorage : IntermediateStorageBase
+{
+    public override string BasePath
+        => Path.Combine(Path.GetTempPath(), nameof(TempIntermediateStorage));
+}
