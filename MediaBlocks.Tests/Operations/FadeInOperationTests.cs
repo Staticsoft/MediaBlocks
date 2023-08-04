f18841f1ff8063f780a57bc0ecf55c02250e7c8d
Oleg Rakovitch <devgeslot@gmail.com>
2023-08-04 02:38:46 -0400

    FadeIn (#3)

---
+++ MediaBlocks.Tests/Operations/FadeInOperationTests.cs
@@ -0,0 +1,25 @@
+﻿using Staticsoft.MediaBlocks.Abstractions;
+using Staticsoft.MediaBlocks.FFMpeg;
+using Staticsoft.TreeOperations.Memory;
+using System.Threading.Tasks;
+
+namespace Staticsoft.MediaBlocks.Tests;
+
+public class FadeInOperationTests : OperationTest
+{
+    protected override TreeProcessorBuilder<MediaReference> Tree(TreeProcessorBuilder<MediaReference> tree) => tree
+        .With<MergeOperation>()
+        .With<FadeInOperation>();
+
+    [Test]
+    public Task FadesInVideo()
+        => Process(
+            FadeIn(
+                Merge(
+                    Image("green.png"),
+                    Audio("beep.mp3")
+                ),
+                250
+            )
+        );
+}
