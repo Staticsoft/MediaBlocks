0509243d35f997584a7b2a20f241c2d5b1f0693d
Oleg Rakovitch <devgeslot@gmail.com>
2023-08-04 22:37:28 -0400

    FadeOut (#4)

---
+++ MediaBlocks.FFMpeg/Operations/FadeProperties.cs
@@ -0,0 +1,9 @@
+﻿using Staticsoft.MediaBlocks.Abstractions;
+
+namespace Staticsoft.MediaBlocks.FFMpeg;
+
+public class FadeProperties
+{
+    public MediaReference Media { get; init; } = new();
+    public int Duration { get; init; } = 0;
+}
---
+++ MediaBlocks.Tests/Operations/FadeOutOperationTests.cs
@@ -0,0 +1,25 @@
+﻿using Staticsoft.MediaBlocks.Abstractions;
+using Staticsoft.MediaBlocks.FFMpeg;
+using Staticsoft.TreeOperations.Memory;
+using System.Threading.Tasks;
+
+namespace Staticsoft.MediaBlocks.Tests;
+
+public class FadeOutOperationTests : OperationTest
+{
+    protected override TreeProcessorBuilder<MediaReference> Tree(TreeProcessorBuilder<MediaReference> tree) => tree
+        .With<MergeOperation>()
+        .With<FadeOutOperation>();
+
+    [Test]
+    public Task FadesOutVideo()
+        => Process(
+            FadeOut(
+                Merge(
+                    Image("green.png"),
+                    Audio("beep.mp3")
+                ),
+                250
+            )
+        );
+}
