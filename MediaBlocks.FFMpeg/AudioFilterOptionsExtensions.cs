02b62e4e47b73abd9a5043640324e208da97ed07
Oleg Rakovitch <devgeslot@gmail.com>
2023-08-04 23:09:17 -0400

    FadeIn and FadeOut for audio

---
+++ MediaBlocks.FFMpeg/AudioFilterOptionsExtensions.cs
@@ -0,0 +1,18 @@
+﻿using FFMpegCore.Arguments;
+
+namespace Staticsoft.MediaBlocks.FFMpeg;
+
+public static class AudioFilterOptionsExtensions
+{
+    public static AudioFilterOptions FadeIn(this AudioFilterOptions options, int duration)
+        => options.Fade(0, duration, "in");
+
+    public static AudioFilterOptions FadeOut(this AudioFilterOptions options, int startTime, int duration)
+        => options.Fade(startTime, duration, "out");
+
+    static AudioFilterOptions Fade(this AudioFilterOptions options, int startTime, int duration, string direction)
+    {
+        options.Arguments.Add(new FadeArgument(startTime, duration, "afade", direction));
+        return options;
+    }
+}
---
+++ MediaBlocks.FFMpeg/FadeArgument.cs
@@ -0,0 +1,26 @@
+﻿using FFMpegCore.Arguments;
+using System;
+
+namespace Staticsoft.MediaBlocks.FFMpeg;
+
+public class FadeArgument : IVideoFilterArgument, IAudioFilterArgument
+{
+    readonly int Duration;
+    readonly int StartFrom;
+    readonly string Direction;
+
+    public FadeArgument(int startFrom, int duration, string fadeFilter, string direction)
+        => (StartFrom, Duration, Key, Direction)
+        = (startFrom, duration, fadeFilter, direction);
+
+    public string Key { get; }
+
+    public string Value
+        => $"type={Direction}:start_time={StartTime:0.###}:duration={Seconds:0.###}";
+
+    float StartTime
+        => Convert.ToSingle(StartFrom) / 1000;
+
+    float Seconds
+        => Convert.ToSingle(Duration) / 1000;
+}
