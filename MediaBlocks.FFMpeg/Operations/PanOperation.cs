985119ffa6865bb5c0176e392fb361896a580a49
Oleg Rakovitch <devgeslot@gmail.com>
2023-08-30 20:43:43 -0400

    Pan operation

---
+++ MediaBlocks.FFMpeg/Operations/PanOperation.cs
@@ -0,0 +1,44 @@
+﻿using FFMpegCore;
+using Staticsoft.GraphOperations.Abstractions;
+using Staticsoft.MediaBlocks.Abstractions;
+using System.Threading.Tasks;
+
+namespace Staticsoft.MediaBlocks.FFMpeg;
+
+public class PanOperation : Operation<PanOperationProperties, MediaReference>
+{
+    readonly IntermediateStorage Storage;
+
+    public PanOperation(IntermediateStorage storage)
+        => Storage = storage;
+
+    protected override async Task<MediaReference> Process(PanOperationProperties properties)
+    {
+        var output = $"{Storage.CreateIntermediateFilePath()}.mp4";
+        var analyzed = await FFProbe.AnalyseAsync(properties.Video);
+        await FFMpegArguments
+            .FromFileInput(properties.Video)
+            .OutputToFile(output, overwrite: false, (options) => options
+                .WithVideoFilters(filters => filters.Pan(properties.Direction, analyzed.PrimaryVideoStream!))
+            )
+            .ProcessAsynchronously();
+
+        return new MediaReference()
+        {
+            Path = output
+        };
+    }
+}
+
+public class PanOperationProperties
+{
+    public string Video { get; init; } = string.Empty;
+    public PanDirection Direction { get; init; }
+}
+
+public enum PanDirection
+{
+    Unknown,
+    LeftToRight,
+    RightToLeft
+}
